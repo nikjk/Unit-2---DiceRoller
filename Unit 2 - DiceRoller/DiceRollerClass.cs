cb24e03223a60065afa808bcfc1d081c4acc8020
nikjk <nik.kuehn@outlook.com>
2023-01-24 20:29:35 -0500

    Add project files.

---
+++ Unit 2 - DiceRoller/DiceRollerClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit2DiceRoller
+{
+    public class RollDice 
+    {
+        public static int DiceRoll() // Declaring the method for the random number generator
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 7); // Creates a random number between 1 and 6
+            return num; // returns the number to the Program when method is called
+        }
+        public static string DiceCombinations(int a, int b) // Delcaring method for control statement. When called in the Program, method will determine which statement to return. 
+        {
+            if (a == 1 && b == 1)
+            {
+                return "Snake Eyes";
+            }
+            else if (a == 1 && b == 2 || a == 2 && b == 1)
+            {
+                return "Ace Deuce";
+            }
+            else if (a == 6 && b == 6)
+            {
+                return "Box Cars";
+            }
+            else if (a + b == 7)
+            {
+                return "You Win!";
+            }
+            else if (a + b == 11)
+            {
+                return "You Win!";
+            }
+            else if (a + b == 2)
+            {
+                return "Craps!";
+            }
+            else if (a + b == 3)
+            {
+                return "Craps!";
+            }
+            else if (a + b == 12)
+            {
+                return "Craps!";
+            }
+            else return "";
+        }
+    }
+}
---
+++ Unit 2 - DiceRoller/Program.cs
@@ -0,0 +1,31 @@
+﻿using Unit2DiceRoller; // granting access to the class for the methods.
+
+Console.WriteLine("Welcome to the Grand Circus Casino!");
+Console.WriteLine("Roll the die and test your luck!");
+
+bool goagain = false; // setting bool value to false for loop
+int roll = 1; // setting the value of the first roll
+do // starting the loop for the dice roll
+{
+    int diceRoll1 = RollDice.DiceRoll(); // Calling the DiceRoll method and generating the first random number and storing the value.
+    int diceRoll2 = RollDice.DiceRoll(); // Calling the DiceRoll method and generating the second random number and storing the value.
+
+    Console.WriteLine("Roll " + roll); // Showing the user what roll they are on.
+    Console.WriteLine($"You rolled a {diceRoll1} and {diceRoll2} ({diceRoll1 + diceRoll2} total)"); // Showing the user what they rolled and what it totaled to. 
+    Console.WriteLine(Unit2DiceRoller.RollDice.DiceCombinations(diceRoll1, diceRoll2)); // Calling the control statements to let the user know what their dice combination means. 
+    roll++; // Increasing the roll value by 1
+    
+
+    Console.WriteLine("Go Again? (y/n)"); // Asking the user if they would like to roll again. 
+    string again = Console.ReadLine(); // Taking the users input
+
+    if (again == "y" || again == "Y") // Control statement the sets the bool value for the loop. 
+    {
+        goagain = true;
+    }
+
+    else
+    {
+        goagain= false;
+    }
+} while (goagain);
